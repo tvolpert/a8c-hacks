7926616eab8b2dd0e56bc699c7e33138c4c7c1ee
Timothy Volpert <34486913+tvolpert@users.noreply.github.com>
2023-08-04 09:32:54 -0500

    Update tinyssr.user.js
    fixed clearing out the last SSR when moving to a new ticket without one

--- tinyssr.user.js
+++ tinyssr.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Collapsible SSR Display
 // @namespace    http://tampermonkey.net/
-// @version      0.9.2
+// @version      1.0
 // @description  create a new, collapsible display for the Woo SSR to make it more readable
 // @author       Timothy Volpert
-// @downloadURL
-// @updateURL
+// @downloadURL  https://github.com/tvolpert/a8c-hacks/raw/main/tinyssr.user.js
+// @updateURL    https://github.com/tvolpert/a8c-hacks/raw/main/tinyssr.user.js
 // @match        https://*.zendesk.com/agent/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=zendesk.com
 // @grant        none
@@ -27,53 +27,53 @@
         let pretty = item.querySelector('.prettyssr .actual');
         // get the text of the SSR
         let theText = item.querySelector('div.custom_field_22871957 textarea')
-        if (theText != null) {
-            var raw = theText.textContent;
-            var currentSiteURL = raw.match(findURL)[0];
+        if (theText != null && theText.textContent != null) {
+            let raw = theText.textContent;
+            let currentSiteURL;
+            if (raw.match(findURL) != null) {currentSiteURL = raw.match(findURL)[0]; }
+            if (raw =='' | typeof(raw) == undefined | raw == null) {
+                console.log('no SSR found!');
+                if (pretty != null){
+                    pretty.innerHTML = 'No SSR provided';
+                }
+            } else if (pretty != null && currentSiteURL != null && pretty.innerHTML.includes(currentSiteURL)) { //if the site URL is already present in our SSR, do nothing
+                console.log('the SSR for ' + currentSiteURL + ' is already in place!');
+            } else if (raw != '') { // only do this if there's something in the SSR box
+                console.log('SSR found, cleaning up')
+                //this is where the actual reformatting happens 
+                //first remove #s used as number signs that might break our formatting
+                let hashies = /#(?=\d+)/gm;
+                raw = raw.replaceAll(hashies, '');
+
+                // make formatted URL a link (if present)
+                let theURL = /(View formatted: )(https:\/\/woocommerce\.com.+)/;
+                let theLink = `<a href="$2" target="_blank" class="button">View formatted</a>`
+                raw = raw.replace(theURL, theLink)
+
+                // reformat SSR with collapsible boxes
+                let newPattern = `<details class="ssr-section"><summary>$1<\/summary>$2<\/details>`;
+                let cooked = raw.replaceAll(pattern, newPattern);
+                cooked = cooked.replaceAll('\n', '</p><p>');
+                cooked = cooked.replaceAll('`', '');
+                //end actual reformatting
+
+                if (typeof(pretty) == 'undefined' | pretty == null) { //if our element doesn't exist yet, make one
+                    console.log('creating a pretty lil element');
+
+                    // create a new element to display the reformatted SSR
+                    let prettySSR = document.createElement('div');
+                    prettySSR.classList = "prettyssr property_box";
+                    prettySSR.innerHTML = '<div><h5>System Status Report (pretty)</h5><div class="actual">' + cooked + '</div></div>';
+
+                    // insert our new element into the sidebar, at the top
+                    let propertyBox = item.querySelector('.property_box');
+                    item.insertBefore(prettySSR, propertyBox);
+                } else { //if element exists but is empty or has a different site, plop the new one in
+                    console.log("new site, refreshing SSR");
+                    pretty.innerHTML = cooked;
+                }
+            } 
         }
-       
-        if (raw =='' | typeof(raw) == undefined | raw == null) {
-            console.log('no SSR found!');
-            if (pretty != null){
-                pretty.innerHTML = 'No SSR provided';
-            }
-        } else if (pretty != null && currentSiteURL != null && pretty.innerHTML.includes(currentSiteURL)) { //if the site URL is already present in our SSR, do nothing
-            console.log('the SSR for ' + currentSiteURL + ' is already in place!');
-        } else if (raw != '') { // only do this if there's something in the SSR box
-            console.log('SSR found, cleaning up')
-            //this is where the actual reformatting happens 
-            //first remove #s used as number signs that might break our formatting
-            let hashies = /#(?=\d+)/gm;
-            raw = raw.replaceAll(hashies, '');
-
-            // make formatted URL a link (if present)
-            let theURL = /(View formatted: )(https:\/\/woocommerce\.com.+)/;
-            let theLink = `<a href="$2" target="_blank" class="button">View formatted</a>`
-            raw = raw.replace(theURL, theLink)
-
-            // reformat SSR with collapsible boxes
-            let newPattern = `<details class="ssr-section"><summary>$1<\/summary>$2<\/details>`;
-            let cooked = raw.replaceAll(pattern, newPattern);
-            cooked = cooked.replaceAll('\n', '</p><p>');
-            cooked = cooked.replaceAll('`', '');
-            //end actual reformatting
-
-            if (typeof(pretty) == 'undefined' | pretty == null) { //if our element doesn't exist yet, make one
-                console.log('creating a pretty lil element');
-
-                // create a new element to display the reformatted SSR
-                let prettySSR = document.createElement('div');
-                prettySSR.classList = "prettyssr property_box";
-                prettySSR.innerHTML = '<div><h5>System Status Report (pretty)</h5><div class="actual">' + cooked + '</div></div>';
-
-                // insert our new element into the sidebar, at the top
-                let propertyBox = item.querySelector('.property_box');
-                item.insertBefore(prettySSR, propertyBox);
-            } else { //if element exists but is empty or has a different site, plop the new one in
-                console.log("new site, refreshing SSR");
-                pretty.innerHTML = cooked;
-            }
-        } 
         
     }
 })();
